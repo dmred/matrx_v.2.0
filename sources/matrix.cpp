ed7afb4a4b6867829d8697ae53296b63c69a46f5
dmred <dmredchenko@gmail.com>
2016-05-19 22:42:18 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,136 +1,230 @@
 #include "matrix.h"
-#include <iostream>
-#include <fstream>
-using namespace std;
-
-	int Isclucheniya::FileNotOpen(){ err = 1; cout <<"error_"<< err <<": file not open\n"; return err; }
-	int Isclucheniya::Razmery() { err = 2; cout << "error_" << err << ": nevernye razmery\n"; return err; }
-	int Isclucheniya::WrongIndex() { err = 3; cout << "error_" << err << ": wrong index\n"; return err; }
-	int Isclucheniya::Samoprisvaivanie() { err = 4; cout << "error_" << err << ": samoprisvaivanie\n"; return err; }
-
-	Matrix::Matrix() : n(0), m(0), p(nullptr) {}
-
-	Matrix::Matrix(int _n, int _m) : n(_n), m(_m){
-		p = new int*[n];
-		for (int i = 0; i < n; i++) { p[i] = new int[m]; }
-	}
-
-	Matrix::Matrix(const Matrix &M) : n(M.n), m(M.m) {
-		p = new int*[n];
-		for (int i = 0; i < n; i++){
-			p[i] = new int[m];
-			for (int j = 0; j < m; j++)
-				p[i][j] = M.p[i][j];
+#include "MatrixException.h"  
+#include <string> 
+#include <iostream>   
+#include <fstream>  
+using namespace std;   
+ 
+template <typename T>
+Matrix<T>::Matrix(unsigned int _rows, unsigned int _columns) : matrix(new T *[_rows]), rows(_rows), columns(_columns) {
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = new T[columns];
+		for (int j = 0; j < columns; j++) {
+			matrix[i][j] = 0;
 		}
 	}
+}
 
-	Matrix::~Matrix()
-	{
-		if (p != nullptr)
-		{
-			for (int i = 0; i < n; i++)
-			{
-				delete[] p[i];
-			}
-			delete[] p;
+template <typename T>
+Matrix<T>::Matrix(const Matrix & x) : matrix(new T *[x.rows]), columns(x.columns), rows(x.rows) {
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = new T[columns];
+		for (int j = 0; j < columns; j++) {
+			matrix[i][j] = x.matrix[i][j];
 		}
 	}
+}
 
-	void Matrix::set_matrix(char* a) const{
-		ifstream fin;
-		fin.open(a);
-		if (!fin.is_open()) {
-			Isclucheniya E;
-			throw E.FileNotOpen();
-		}
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < m; j++)
-				fin >> p[i][j];
+template <typename T>
+Matrix<T>::Matrix(T **matr, unsigned int _rows, unsigned int _columns) : matrix(new T *[_rows]), rows(_rows), columns(_columns) {
+	for (int i = 0; i < rows; i++) {
+		matrix[i] = new T[columns];
+		for (int j = 0; j < columns; j++) {
+			matrix[i][j] = matr[i][j];
 		}
 	}
+}
+template <typename T>
+bool Matrix<T>::readFromFile(char* path) {
+	ifstream stream;
+	try {
+		stream.open(path);
 
-	void Matrix::print() const{
-		for (int i = 0; i < n; i++){
-			for (int j = 0; j < m; j++)
-				cout << p[i][j] << " ";
-			cout << "\n";
-		}
-	}
+		if (stream.is_open()) {
+			unsigned int rows, columns;
 
-	Matrix Matrix::operator +(const Matrix &M2) const{
-		if ((n != M2.n) || (m != M2.m)) {
-			Isclucheniya E;
-			throw E.Razmery();
-		}
-		Matrix M_res(M2.n, M2.m);
-		for (int i = 0; i < M2.n; i++)
-		for (int j = 0; j < M2.m; j++)
-			M_res.p[i][j] = p[i][j] + M2.p[i][j];
-		cout << "M1+M2:\n";  M_res.print();
-		return M_res;
-	}
-	Matrix Matrix::operator *(const Matrix &M2) const{
-		if (m != M2.n){ 
-			Isclucheniya E;
-			throw E.Razmery();
-	}
-		Matrix M_res(n, M2.m);
-		for (int i = 0; i < n; i++)
-		for (int j = 0; j < M2.m; j++)
-		{
-			M_res.p[i][j] = 0;
-			for (int k = 0; k < M2.m; k++)
-				M_res.p[i][j] += (p[i][k] * M2.p[k][j]);
-		}
-		cout << "M1*M2:\n";  M_res.print();
-		return M_res;
-	}
-
-	int* Matrix::operator [] (int k) const{
-		if ((k < 0) || (k >= n)){ Isclucheniya E; throw E.WrongIndex(); }
-		int *r = new int[m];
-		for (int j = 0; j < m; j++){
-			r[j] = p[k][j];
-		}
-		return r;
-	}
-
-	int Matrix::getnumstr() const{
-		return n;
-	}
-
-	int Matrix::getnumcol() const{
-		return m;
-	}
-
-	Matrix& Matrix::operator = (const Matrix &M) {
-		if (this == &M){
-		Isclucheniya E; throw E.Samoprisvaivanie(); 	
-		}
-			if (p != nullptr){
-				for (int i = 0; i < n; i++){
-					delete[] p[i];
+			stream >> rows >> columns;
+			T **mass = new T*[rows];
+			for (int i = 0; i < rows; i++) {
+				mass[i] = new T[columns];
+				for (int j = 0; j < columns; j++) {
+					stream >> mass[i][j];
 				}
-				delete[] p;
 			}
-			n = M.n;
-			m = M.m;
-			p = new int*[n];
-			for (int i = 0; i < n; i++){
-				p[i] = new int[m];
-				for (int j = 0; j < m; j++) { p[i][j] = M.p[i][j]; }
+
+			this->matrix = mass;
+			this->rows = rows;
+			this->columns = columns;
+			stream.close();
+
+			return true;
+		}
+		else {
+			throw initException();
+		}
+	}
+	catch (const std::exception& e) {
+		cout << e.what() << '\n';
+		return false;
+	}
+	return false;
+}
+
+template <typename T>
+unsigned int Matrix<T>::rowsNumber() const {
+	return rows;
+}
+
+template <typename T>
+unsigned int Matrix<T>::columnsNumber() const {
+	return columns;
+}
+
+template <typename T>
+Matrix<T> & Matrix<T>::operator =(const Matrix<T>& m2) {
+	if (this != &m2) {
+		(Matrix(m2)).swap(*this);
+	}
+	return *this;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator +(const Matrix<T> &m2) {
+	if (m2.columns != this->columns || m2.rows != this->rows) {
+		throw incompatibleException();
+	}
+	Matrix temp(this->rows, this->columns);
+	for (int i = 0; i < this->rows; i++) {
+		for (int j = 0; j < this->columns; j++) {
+			temp[i][j] = m2.matrix[i][j] + this->matrix[i][j];
+		}
+	}
+	return temp;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator -(const Matrix<T> &m2) {
+	if (m2.columns != this->columns || m2.rows != this->rows) {
+		throw incompatibleException();
+	}
+	Matrix temp(this->rows, this->columns);
+	for (int i = 0; i < this->rows; i++) {
+		for (int j = 0; j < this->columns; j++) {
+			temp.matrix[i][j] = matrix[i][j] - m2.matrix[i][j];
+		}
+	}
+	return temp;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator *(double num) {
+	if (this->matrix == nullptr || this->rows == 0 || this->columns == 0) {
+		throw emptyException();
+	}
+	Matrix temp(this->rows, this->columns);
+	for (int i = 0; i < this->rows; i++) {
+		for (int j = 0; j < this->columns; j++) {
+			temp.matrix[i][j] = this->matrix[i][j] * num;
+		}
+	}
+	return temp;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator *(const Matrix &m2) {
+	if (this->columns != m2.rows) {
+		throw incompatibleException();
+	}
+	Matrix temp(this->rows, m2.columns);
+
+	T t = 0;
+
+	for (int row = 0; row < this->rows; row++) {
+		for (int col = 0; col < m2.columns; col++) {
+			t = 0;
+			for (int inner = 0; inner < this->columns; inner++) {
+				t = t + this->matrix[row][inner] * m2.matrix[inner][col];
 			}
-			return *this;
+			temp.matrix[row][col] = t;
+		}
+	}
+	return temp;
+}
+
+template <typename T>
+bool Matrix<T>::operator ==(const Matrix & m2) {
+	if (rows != m2.rows || columns != m2.columns) {
+		return false;
 	}
 
-	bool Matrix::operator ==(const Matrix &M2) const{
-		if (n != M2.n || m != M2.m)
-			return false; 
-
-		for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-		if (p[i][j] != M2.p[i][j])
-			return false;
-
-		return true; 
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			if (matrix[i][j] != m2.matrix[i][j]) {
+				return false;
+			}
+		}
 	}
+
+	return true;
+}
+
+template <typename T>
+T* Matrix<T>::operator [](unsigned int index) {
+	if (index < 0 || index > this->rows) {
+		throw indexException();
+	}
+	if (rows == 0 || matrix == nullptr) {
+		throw emptyException();
+	}
+
+	return this->matrix[index];
+}
+
+template <typename T>
+void Matrix<T>::swap(Matrix & x) {
+	std::swap(x.matrix, matrix);
+	std::swap(x.columns, columns);
+	std::swap(x.rows, rows);
+}
+
+template <typename T>
+Matrix<T>::~Matrix() {
+	if (matrix != nullptr) {
+		for (int i = 0; i < rows; i++) {
+			delete[] matrix[i];
+		}
+		delete[] matrix;
+	}
+}
+
+template <typename T>
+std::ostream & operator <<(std::ostream & os, const Matrix<T> & x) {
+	if (x.columns == 0 || x.rows == 0 || x.matrix == nullptr) {
+		throw emptyException();
+	}
+	for (int i = 0; i < x.rows; ++i) {
+		for (int j = 0; j < x.columns; ++j) {
+			os.width(4);
+			os << x.matrix[i][j];
+		}
+		os << std::endl;
+	}
+
+	return os;
+}
+
+template <typename T>
+std::istream & operator >>(std::istream & input, Matrix<T> & matrix) {
+	for (int i = 0; i < matrix.rows; ++i) {
+		for (int j = 0; j < matrix.columns; ++j) {
+		
+				if (!(input >> matrix.matrix[i][j])) {
+					throw initException();
+				}
+			
+			
+		}
+	}
+
+	return input;
+}
